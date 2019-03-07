07109d98da7afa44aa151c1f4067bc6f1229beaa
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 09:52:17 -0600

    adding a README. The example currently doesn't work but I am fixing it.

--- R/make-roc.r
+++ R/make-roc.r
@@ -9,8 +9,15 @@
 #' roc()
 
 make_roc <- function(data, predictor, known_class) {
+  # use tidy eval
+  predictor <- rlang::enquo(predictor)
+  known_class <- rlang::enquo(known_class)
+
+  pred_values <- rlang::eval_tidy(predictor, data)
+  known_values <- rlang::eval_tidy(known_class, data)
+
   # get binary classification values
-  df <- measure_perf(data, predictor, known_class)
+  df <- measure_perf(data,  pred_values,  pred_values)
 
   # add true positive rate and false positive rate to the data-frame `data`
   # add true positive = 0 and false positive = 0 to make sure an ROC curve always starts at 0, 0
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# tidyroc
+tidyroc
+=======
+
+This a repository for an R package `tidyroc`. `tidyroc` is currently
+under development, and I plan to release it this summer (summer 2019).
+
+Usage
+=====
+
+`tidyroc` has 3 primary functions:
+
+-   to generate values to plot a receiver operating characteristic (ROC)
+    curve  
+-   to generate values to plot a precision-recall curve  
+-   to calculate areas under an ROC and precision-recall curves (AUC)
+
+`tidyroc` is designed to be integrated with the Tidyverse. It is intended to work with `broom` and `ggplot2`. 
+
+
+
+
