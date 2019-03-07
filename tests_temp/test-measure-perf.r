5ffc2af06fdf85e79885ac2e354eadb45f607518
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 09:21:10 -0600

    testing measure_perf() unofficially

--- tests_temp/test-measure-perf.r
+++ tests_temp/test-measure-perf.r
@@ -11,19 +11,16 @@ d1 <- glm(Species ~ Petal.Width + Petal.Length + Sepal.Width,
          data = iris.small,
          family = binomial) %>%
   augment() %>%
-  calc_roc(predictor = .fitted, positive = Species) %>%
   mutate(model = "model1")
 
 d2 <- glm(Species ~ Petal.Width,
          data = iris.small,
          family = binomial) %>%
   augment() %>%
-  calc_roc(predictor = .fitted, positive = Species) %>%
   mutate(model = "model2")
 
 d <- bind_rows(d1, d2)
 
-d %>%
-  ggplot(aes(false_pos, true_pos, color = model)) +
-  geom_line() +
-  geom_point()
+test <- d %>%
+  group_by(model) %>%
+  make_roc(predictor = .fitted, known_class = Species)
