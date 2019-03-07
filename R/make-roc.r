1cde0c290e2008830fba586e436d853ba7ac65e3
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 10:08:54 -0600

    make ROC operate on groups

--- R/make-roc.r
+++ R/make-roc.r
@@ -8,7 +8,7 @@
 #' @examples
 #' roc()
 
-make_roc <- function(data, predictor, known_class) {
+make_roc_ungrouped <- function(data, key, predictor, known_class) {
   # use tidy eval
   predictor <- rlang::enquo(predictor)
   known_class <- rlang::enquo(known_class)
@@ -17,12 +17,15 @@ make_roc <- function(data, predictor, known_class) {
   known_values <- rlang::eval_tidy(known_class, data)
 
   # get binary classification values
-  df <- measure_perf(data, pred_values, known_values)
+  df <- measure_perf(data = data, predictor = pred_values, known_class = known_values)
 
   # add true positive rate and false positive rate to the data-frame `data`
   # add true positive = 0 and false positive = 0 to make sure an ROC curve always starts at 0, 0
   df %>%
-    dplyr::select(tpr, fpr) %>%
-    dplyr::add_row(true_pos = 0, false_pos = 0) %>%
+    dplyr::add_row(tpr = 0, fpr = 0) %>%
     dplyr::arrange(fpr, tpr) # order output by false positive and then true positive rate to plot an ROC curve correctly
+}
+
+make_roc <- function(data, ...) {
+  group_map(data, make_roc_ungrouped, ...)
 }
