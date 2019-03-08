1b2c081c8ddae3075828671237130d74d7170c56
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 21:37:51 -0600

    adding an anchor for precision-recall curves

--- R/make-pr.r
+++ R/make-pr.r
@@ -23,6 +23,7 @@ make_pr_ungrouped <- function(data, key, predictor, known_class) {
   # add recall and precision to the data-frame `data`
   df %>%
     dplyr::select(-c(tnr, fpr, fnr), recall = tpr, precision = ppv) %>% # remove extra columns to keep data-frames simpler
+    dplyr::add_row(recall = 0, precision = 1) %>% # add recall = 0 and precision = 1 to make sure a precision-recall curve is anchored at 0, 1
     dplyr::arrange(recall, desc(precision)) # order output by recall (ascending order) and then precision (descending order) to plot the precision-recall curve correctly
 }
 
