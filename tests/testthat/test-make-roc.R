fb3b0c35d47512ebb8420ae3d15afa35cc638712
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 20:11:24 -0600

    updating test to make an ROC curve

--- tests/testthat/test-make-roc.R
+++ tests/testthat/test-make-roc.R
@@ -2,11 +2,13 @@ context("test-make-roc")
 
 test_that("ROC curve values are correct", {
 
+  # predictor perfectly separate the two outcomes
   df <- data.frame(
     predictor = c(0, 0, 0, 10, 10, 10),
     known_class = c(0, 0, 0, 1, 1, 1) # 1 is positive, 0 is negative
   )
 
+  # ROC should be a right angle
   expect_equal(
     df %>% make_roc(predictor, known_class),
     df %>%
@@ -17,6 +19,27 @@ test_that("ROC curve values are correct", {
       cbind(
         tpr = c(0, 1, 1, 1, 1, 1, 1),
         fpr = c(0, 1, 1, 1, 0, 0, 0)
+      ) %>%
+      arrange(fpr, tpr)
+  )
+
+  # predictor is a random classifier of two outcomes
+  df <- data.frame(
+    predictor = c(0, 0, 0, 0, 0, 0),
+    known_class = c(0, 0, 0, 1, 1, 1) # 1 is positive, 0 is negative
+  )
+
+  # ROC should be a diagonal
+  expect_equal(
+    df %>% make_roc(predictor, known_class),
+    df %>%
+      add_row(
+        predictor = NA,
+        known_class = NA,
+        .before = 1) %>%
+      cbind(
+        tpr = c(0, 1, 1, 1, 1, 1, 1),
+        fpr = c(0, 1, 1, 1, 1, 1, 1)
       ) %>%
       arrange(fpr, tpr)
   )
