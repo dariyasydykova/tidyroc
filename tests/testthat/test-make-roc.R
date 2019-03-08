fc2034942484b19648a4b942d5af174dc3714226
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-07 20:35:25 -0600

    adding a test to handle -Inf

--- tests/testthat/test-make-roc.R
+++ tests/testthat/test-make-roc.R
@@ -44,4 +44,25 @@ test_that("ROC curve values are correct", {
       arrange(fpr, tpr)
   )
 
+  # predictor contains infinity
+  df <- data.frame(
+    predictor = c(-Inf, 0, 0, 10, 10, 10),
+    known_class = c(0, 0, 0, 1, 1, 1) # 1 is positive, 0 is negative
+  )
+
+  # ROC should be a right angle
+  expect_equal(
+    df %>% make_roc(predictor, known_class),
+    df %>%
+      add_row(
+        predictor = NA,
+        known_class = NA,
+        .before = 1) %>%
+      cbind(
+        tpr = c(0, 1, 1, 1, 1, 1, 1),
+        fpr = c(0, 1, 2/3, 2/3, 0, 0, 0)
+      ) %>%
+      arrange(fpr, tpr)
+  )
+
 })
