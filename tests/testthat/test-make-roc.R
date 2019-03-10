920af01dd8633b10278ab0c9a7d2fa13cbbf3832
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-10 18:47:46 -0500

    Adding NA tests

--- tests/testthat/test-make-roc.R
+++ tests/testthat/test-make-roc.R
@@ -65,4 +65,46 @@ test_that("ROC curve values are correct", {
       arrange(fpr, tpr)
   )
 
+  # predictor contains NA
+  df <- data.frame(
+    predictor = c(0, 0, 0, NA, 0, 0, 0),
+    known_class = c(0, 0, 0, 1, 1, 1, 1) # 1 is positive, 0 is negative
+  )
+
+  # tpr and fpr should be NA when predictor = NA
+  expect_equal(
+    df %>% make_roc(predictor, known_class),
+    df %>%
+      add_row(
+        predictor = NA,
+        known_class = NA,
+        .before = 1) %>%
+      cbind(
+        tpr = c(0, 1, 1, 1, NA, 1, 1, 1),
+        fpr = c(0, 1, 1, 1, NA, 1, 1, 1)
+      ) %>%
+      arrange(fpr, tpr)
+  )
+
+  # known class contains NA
+  df <- data.frame(
+    predictor = c(0, 0, 0, 0, 0, 0, 0),
+    known_class = c(0, 0, 0, NA, 1, 1, 1) # 1 is positive, 0 is negative
+  )
+
+  # tpr and fpr should be NA when known_class = NA
+  expect_equal(
+    df %>% make_roc(predictor, known_class),
+    df %>%
+      add_row(
+        predictor = NA,
+        known_class = NA,
+        .before = 1) %>%
+      cbind(
+        tpr = c(0, 1, 1, 1, NA, 1, 1, 1),
+        fpr = c(0, 1, 1, 1, NA, 1, 1, 1)
+      ) %>%
+      arrange(fpr, tpr)
+  )
+
 })
