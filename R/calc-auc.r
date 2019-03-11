2caf1ce78116de3e4cb775c1ddb7b50cb39b11f6
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-03-10 19:37:27 -0500

    added an example and update to tidyverse style guide coding

--- R/calc-auc.r
+++ R/calc-auc.r
@@ -7,14 +7,8 @@
 #' @keywords
 #' @export
 #' @examples
-#' # load tidyverse packages
 #' library(tidyverse)
 #' library(broom)
-#'
-#' # load cowplot to change plot theme
-#' library(cowplot)
-#'
-#' # load tidyroc
 #' library(tidyroc)
 #'
 #' # get `biopsy` dataset from `MASS`
@@ -22,40 +16,40 @@
 #'
 #' # change column names from `V1`, `V2`, etc. to informative variable names
 #' colnames(biopsy) <-
-#'  c("ID",
-#'    "clump_thickness",
-#'    "uniform_cell_size",
-#'    "uniform_cell_shape",
-#'    "marg_adhesion",
-#'    "epithelial_cell_size",
-#'    "bare_nuclei",
-#'    "bland_chromatin",
-#'    "normal_nucleoli",
-#'    "mitoses",
-#'    "outcome")
+#'   c(
+#'     "ID",
+#'     "clump_thickness",
+#'     "uniform_cell_size",
+#'     "uniform_cell_shape",
+#'     "marg_adhesion",
+#'     "epithelial_cell_size",
+#'     "bare_nuclei",
+#'     "bland_chromatin",
+#'     "normal_nucleoli",
+#'     "mitoses",
+#'     "outcome"
+#'   )
 #'
 #' # fit a logistic regression model to predict tumour type
 #' glm(outcome ~ clump_thickness + uniform_cell_shape,
-#'    family = binomial,
-#'    data = biopsy
+#'   family = binomial,
+#'   data = biopsy
 #' ) %>%
-#'  augment() %>% # use broom to add glm output to the #' original data frame
-#'  make_roc(predictor = .fitted, known_class = outcome) %>% # get values to plot an ROC curve
-#'  summarise(auc = calc_auc(x = fpr, y = tpr)) # calculate the area under an ROC curve
-#'
-
+#'   augment() %>% # use broom to add glm output to the #' original data frame
+#'   make_roc(predictor = .fitted, known_class = outcome) %>% # get values to plot an ROC curve
+#'   summarise(auc = calc_auc(x = fpr, y = tpr)) # calculate the area under an ROC curve
 calc_auc <- function(x, y) {
   # get the width between each point to get the width of the rectangle
-  width = abs(lead(x) - x)
+  width <- abs(lead(x) - x)
 
   # get the height of the rectanlge
-  height1 = y
+  height1 <- y
 
   # get the height of the triangle
-  height2 = abs(lead(y) - y)
+  height2 <- abs(lead(y) - y)
 
   # calculate the area of the rectanlge and the triangle
-  area = width*(height1 + height2 * 0.5)
+  area <- width * (height1 + height2 * 0.5)
 
   # sum all the rectangles and triangles
   sum(area, na.rm = TRUE)
